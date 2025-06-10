97d665efb38d381b612b5f962f23030f1c2dd45e
Andrei Sosnin <An.S-1@yandex.ru>
2025-06-10 03:23:26 +0300

    v2_OOP commit 1

---
+++ src/main/java/ru/practicum/calorieCounter/model/StepTracker.java
@@ -0,0 +1,8 @@
+package ru.practicum.calorieCounter.model;
+
+public class StepTracker {
+    private String month;
+    private Integer day;
+    private Integer step;
+
+}
---
+++ src/main/java/ru/practicum/calorieCounter/repository/MonthDataRepository.java
@@ -0,0 +1,4 @@
+package ru.practicum.calorieCounter.repository;
+
+public class MonthDataRepository {
+}
